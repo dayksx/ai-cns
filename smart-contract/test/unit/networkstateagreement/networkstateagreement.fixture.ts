25122b6496e529bc5e662375cba3e4a4c013df57
Frederic HENG <frederic.heng@consensys.net>
2025-03-17 22:46:52 +0100

    test(networkstateagreement): add unit tests for signing agreement

--- smart-contract/test/unit/networkstateagreement/networkstateagreement.fixture.ts
+++ smart-contract/test/unit/networkstateagreement/networkstateagreement.fixture.ts
@@ -1,18 +1,18 @@
 import { ethers } from "hardhat";
 
 import type { NetworkStateAgreement } from "../../../src/types/NetworkStateAgreement";
-import type { NetworkStateAgreement__factory } from "../../../src/types/factories/NetworkStateAgreementn__factory";
+import type { NetworkStateAgreement__factory } from "../../../src/types/factories/NetworkStateAgreement__factory";
 
 export async function deployNetworkStateAgreementFixture() {
-  const message = "IPFS hash";
+  const constitutionHash = "IPFS hash";
 
-  const [owner, account2] = await ethers.getSigners();
+  const [owner, user1] = await ethers.getSigners();
 
   const NetworkStateAgreement = (await ethers.getContractFactory(
     "NetworkStateAgreement",
   )) as NetworkStateAgreement__factory;
-  const networkStateAgreement = (await NetworkStateAgreement.deploy(message)) as NetworkStateAgreement;
+  const networkStateAgreement = (await NetworkStateAgreement.deploy(constitutionHash)) as NetworkStateAgreement;
   const networkStateAgreementAddress = await networkStateAgreement.getAddress();
 
-  return { networkStateAgreement, networkStateAgreementAddress, message, owner, account2 };
+  return { networkStateAgreement, networkStateAgreementAddress, constitutionHash, owner, user1 };
 }
