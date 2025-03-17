a74aec77f376065ce79a8991cdec1d8a886458be
Frederic HENG <frederic.heng@consensys.net>
2025-03-17 22:46:52 +0100

    test(networkstateagreement): add unit tests for signing agreement

--- smart-contract/test/unit/networkstateagreement/networkstateagreement.ts
+++ smart-contract/test/unit/networkstateagreement/networkstateagreement.ts
@@ -11,27 +11,39 @@ describe("NetworkStateAgreement", function () {
 
     const signers = await ethers.getSigners();
     this.signers.admin = signers[0];
+    this.signers.user1 = signers[1];
 
     this.loadFixture = loadFixture;
   });
 
   describe("Testing", function () {
     beforeEach(async function () {
-      const { networkStateAgreement, networkStateAgreementAddress, message, owner, account2 } = await this.loadFixture(
+      const { networkStateAgreement, networkStateAgreementAddress, constitutionHash, owner } = await this.loadFixture(
         deployNetworkStateAgreementFixture,
       );
       this.networkStateAgreement = networkStateAgreement;
       this.networkStateAgreementAddress = networkStateAgreementAddress;
-      this.message = message;
+      this.constitutionHash = constitutionHash;
       this.owner = owner;
-      this.account2 = account2;
     });
 
     it("should return the new IPFS hash once it's changed", async function () {
       expect(await this.networkStateAgreement.connect(this.signers.admin).constitutionHash()).to.equal("IPFS hash");
-
-      await this.networkStateAgreement.updateConstitution("new IPFS hash");
+      await this.networkStateAgreement.connect(this.signers.admin).updateConstitution("new IPFS hash");
       expect(await this.networkStateAgreement.connect(this.signers.admin).constitutionHash()).to.equal("new IPFS hash");
+    });
+
+    it("should allow a user to sign the agreement", async function () {
+      await this.networkStateAgreement.connect(this.signers.user1).signAgreement();
+      const hasSigned = await this.networkStateAgreement.hasAgreed(this.signers.user1);
+      expect(hasSigned).to.equal(true);
+    });
+
+    it("should not allow a user to sign the agreement twice", async function () {
+      await this.networkStateAgreement.connect(this.signers.user1).signAgreement();
+      await expect(this.networkStateAgreement.connect(this.signers.user1).signAgreement()).to.be.revertedWith(
+        "Agreement already signed",
+      );
     });
   });
 });
