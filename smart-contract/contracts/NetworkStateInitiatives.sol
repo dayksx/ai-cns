0e3a0eae4bff741dfafa90c9012bec95597de07d
Frederic HENG <frederic.heng@consensys.net>
2025-03-18 01:35:48 +0100

    feat(networkstateinitiatives): init the Initiatives contract

---
+++ smart-contract/contracts/NetworkStateInitiatives.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.19;
+
+contract NetworkStateInitiatives {
+    // Struct to store initiatives properties
+    struct Initiative {
+        bytes32 id;
+        address instigator;
+        string title;
+        string description;
+        string category;
+        string[] tags;
+        uint256 timestamp;
+    }
+
+    address public owner; // Owner of the contract
+    Initiative[] public initiatives; // Array of initiatives
+
+    // Event emitted when a new initiative is created
+    event InitiativeCreated(
+        bytes32 initiativeId,
+        address instigator,
+        string title,
+        string description,
+        string category
+    );
+
+    /**
+     * @dev Modifier to make a function callable only by the owner.
+     * Reverts with a custom error message if the caller is not the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this");
+        _;
+    }
+
+    /**
+     * @dev Constructor that sets the initial constitution hash and assigns the contract owner.
+     */
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Creates a new initiative with the given details and stores it in the initiatives array.
+     * @param _title The title of the initiative.
+     * @param _description A brief description of the initiative.
+     * @param _category The category under which the initiative falls.
+     * @param _tags An array of tags associated with the initiative.
+     */
+    function createInitiatives(
+        string memory _title,
+        string memory _description,
+        string memory _category,
+        string[] memory _tags
+    ) public {
+        bytes32 initiativeId = generateUUID();
+        Initiative memory newInitiative = Initiative({
+            id: initiativeId,
+            instigator: msg.sender,
+            title: _title,
+            description: _description,
+            category: _category,
+            tags: _tags,
+            timestamp: block.timestamp
+        });
+        initiatives.push(newInitiative);
+        emit InitiativeCreated(initiativeId, msg.sender, _title, _description, _category);
+    }
+
+    /**
+     * @dev Generates a unique identifier (UUID) based on the current block's timestamp,
+     * previous block's random number, sender's address, and the hash of the previous block.
+     * This method is an acceptable pseudo-random and deterministic function for generating our UUIDs.
+     * @return bytes32 A unique identifier generated using keccak256 hash function.
+     */
+    function generateUUID() public view returns (bytes32) {
+        return keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender, blockhash(block.number - 1)));
+    }
+}
--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -2,11 +2,14 @@ import type { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/dist/src
 import { ethers } from "hardhat";
 
 import type { NetworkStateAgreement } from "../src/types/NetworkStateAgreement";
+import type { NetworkStateInitiatives } from "../src/types/NetworkStateInitiatives";
 import type { NetworkStateAgreement__factory } from "../src/types/factories/NetworkStateAgreement__factory";
+import type { NetworkStateInitiatives__factory } from "../src/types/factories/NetworkStateInitiatives__factory";
 
 async function main() {
   const signers: SignerWithAddress[] = await ethers.getSigners();
-
+  console.log("Deployer:", signers[0].address);
+/*
   const NetworkStateAgreement = (await ethers.getContractFactory(
     "NetworkStateAgreement",
   )) as NetworkStateAgreement__factory;
@@ -16,6 +19,16 @@ async function main() {
   networkStateAgreement.waitForDeployment();
   const networkStateAgreementAddress = await networkStateAgreement.getAddress();
   console.log("NetworkStateAgreement deployed to:", networkStateAgreementAddress);
+*/
+  const NetworkStateInitiatives = (await ethers.getContractFactory(
+    "NetworkStateInitiatives",
+  )) as NetworkStateInitiatives__factory;
+  const networkStateInitiatives = (await NetworkStateInitiatives.connect(
+    signers[1],
+  ).deploy()) as NetworkStateInitiatives;
+  networkStateInitiatives.waitForDeployment();
+  const networkStateInitiativesAddress = await networkStateInitiatives.getAddress();
+  console.log("NetworkStateInitiatives deployed to:", networkStateInitiativesAddress);
 }
 
 main().catch((error) => {
