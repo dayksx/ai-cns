c7802788d1076e72c9d4ff626346a28a7ae40fcb
Frederic HENG <frederic.heng@consensys.net>
2025-03-19 12:45:50 +0100

    feat(datatest): add script to fill SC with data when deploying

--- smart-contract/scripts/deploy-Initiatives.ts
+++ smart-contract/scripts/deploy-Initiatives.ts
@@ -9,11 +9,14 @@ export async function deployNetworkStateInitiatives(
   deployer: SignerWithAddress,
   verifyContract: boolean = true,
 ): Promise<string> {
+  console.log("Deployer:", deployer.address);
   const factory = (await ethers.getContractFactory("NetworkStateInitiatives")) as NetworkStateInitiatives__factory;
   const contract = (await factory.connect(deployer).deploy()) as NetworkStateInitiatives;
   await contract.waitForDeployment();
   const address = await contract.getAddress();
   console.log("NetworkStateInitiatives deployed to:", address);
+
+  await fillData(contract);
 
   if (verifyContract) {
     setTimeout(async () => {
@@ -22,4 +25,40 @@ export async function deployNetworkStateInitiatives(
   }
 
   return address;
+}
+
+async function fillData(contract: NetworkStateInitiatives) {
+  const [acc1, acc2, acc3] = await ethers.getSigners();
+  const tags = ["identity", "privacy", "zero-knowledge proof", "decentralization"];
+  await contract
+    .connect(acc1)
+    .createInitiatives(
+      acc1.address,
+      "Cryptographical Passport with zkproof",
+      "A decentralized identity verification system that utilizes zero-knowledge proofs to allow CNS netizens to prove their identity or certain attributes without revealing sensitive information. This digital passport empowers users to interact securely across decentralized platforms while protecting their personal data.",
+      "Decentralized Identity Systems",
+      tags,
+    );
+
+  const tags2 = ["dispute resolution", "decentralization", "community governance"];
+  await contract
+    .connect(acc2)
+    .createInitiatives(
+      acc2.address,
+      "Decentralized Dispute Resolution System",
+      "A system where disputes are resolved through a network of autonomous agents and community-driven protocols utilizing blockchain technology to ensure transparency and fairness. Smart contracts will automate resolutions based on predefined community guidelines integrating decentralized identity verification and leveraging reputation systems to provide a secure and equitable process.",
+      "Governance",
+      tags2,
+    );
+
+  const tags3 = ["crowfunding", "decentralization"];
+  await contract
+    .connect(acc3)
+    .createInitiatives(
+      acc3.address,
+      "Decentralized crodwfunding platform",
+      "A system where crowfunding is accessible by everyone while relaying on web3 technologies.",
+      "crowfunding",
+      tags3,
+    );
 }
