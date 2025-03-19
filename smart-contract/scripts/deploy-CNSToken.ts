a2ca136001d1b6c886df6679db330582a11634f6
Frederic HENG <frederic.heng@consensys.net>
2025-03-20 00:32:21 +0100

    feat(cns token): add CNS Token ERC20 for testing

---
+++ smart-contract/scripts/deploy-CNSToken.ts
@@ -0,0 +1,24 @@
+import type { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/dist/src/signer-with-address";
+import { ethers } from "hardhat";
+
+import type { CNSToken } from "../src/types/CNSToken";
+import type { CNSToken__factory } from "../src/types/factories/CNSToken__factory";
+import { verify } from "./VerifyContract";
+
+export async function deployCNSToken(deployer: SignerWithAddress, verifyContract: boolean = true): Promise<string> {
+  const MINTER_ADDRESS = "0x01f8e269cadcd36c945f012d2eeae814c42d1159";
+  console.log("Deployer:", deployer.address);
+  const factory = (await ethers.getContractFactory("CNSToken")) as CNSToken__factory;
+  const contract = (await factory.connect(deployer).deploy(deployer.address, MINTER_ADDRESS)) as CNSToken;
+  await contract.waitForDeployment();
+  const address = await contract.getAddress();
+  console.log("CNSToken deployed to:", address);
+
+  if (verifyContract) {
+    setTimeout(async () => {
+      await verify(address, [deployer.address, MINTER_ADDRESS]);
+    }, 10000);
+  }
+
+  return address;
+}
