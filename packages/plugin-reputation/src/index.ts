a01bd7b3486632fe214a73781be517ffba34cece
dayksx <dayksx@protonmail.com>
2025-05-20 12:26:22 +0200

    fetch top  holders

--- packages/plugin-reputation/src/index.ts
+++ packages/plugin-reputation/src/index.ts
@@ -2,6 +2,8 @@ import type { Plugin } from "@elizaos/core";
 
 import { issueAttestationAction } from "./actions/issueAttestation.ts"
 import { attestationsProvider } from "./providers/attestations.ts"
+import { cnsBalanceProvider } from "./providers/cnsBalance.ts"
+import { topHoldersProvider } from "./providers/topHolders.ts"
 import { tipCNSTokenAction } from "./actions/tipCNSToken.ts";
 
 export const reputationPlugin: Plugin = {
@@ -9,6 +11,6 @@ export const reputationPlugin: Plugin = {
     description: "Issue points and attestations for netizens",
     actions: [issueAttestationAction, tipCNSTokenAction],
     evaluators: [],
-    providers: [attestationsProvider],
+    providers: [attestationsProvider, cnsBalanceProvider, topHoldersProvider],
 };
 export default reputationPlugin;
--- packages/plugin-reputation/src/providers/index.ts
+++ packages/plugin-reputation/src/providers/index.ts
@@ -1,1 +1,2 @@
 export * from "./attestations.ts"
+export * from "./topHolders.ts"
